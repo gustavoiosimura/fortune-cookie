9b8e6ae794b9805837bfb22568f13c905901b75b
Gustavo Iosimura <guiosimura@hotmail.com>
2022-12-29 11:47:35 -0300

    Created test for App component with Jest

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const goodLuck = screen.getByText("Good luck!");
+  expect(goodLuck).toBeInTheDocument();
 });
+
+ 
